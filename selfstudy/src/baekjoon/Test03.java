91b1a559a33ba625287db784cd519828f71e2ea4
smy984 <smy984@naver.com>
2022-07-30 14:45:54 +0900

    고양

---
+++ selfstudy/src/baekjoon/Test03.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+public class Test03 {
+	public static void main(String[] args) {
+//		아래 예제와 같이 고양이를 출력하시오.
+		
+//		\    /\
+//		 )  ( ')
+//		(  /  )
+//		 \(__)|
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
