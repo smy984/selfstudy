f0693118942e42fd8442a6038530ed182758c706
smy984 <smy984@naver.com>
2022-07-30 14:58:47 +0900

    개

---
+++ selfstudy/src/baekjoon/Test04.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+public class Test04 {
+	public static void main(String[]args) {
+//		아래 예제와 같이 개를 출력하시오.
+
+//		|\_/|
+//		|q p|   /}
+//		( 0 )"""\
+//		|"^"`    |
+//		||_/=\\__|
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`\t |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
