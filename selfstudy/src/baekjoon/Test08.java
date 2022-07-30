bdc540d197ebe19966a6b025c75070bca2c2b242
smy984 <smy984@naver.com>
2022-07-30 15:26:25 +0900

    A/B

---
+++ selfstudy/src/baekjoon/Test08.java
@@ -0,0 +1,20 @@
+package baekjoon;
+import java.util.Scanner;
+public class Test08 {
+	public static void main(String[] args) {
+//		두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+//
+//		첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//		첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+
+		System.out.print("(0 < A, B < 10) ");
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a / b);
+	}
+}
