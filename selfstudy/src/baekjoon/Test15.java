45c643dcbf5cb8d5948a61ea285596f728ef51f3
smy984 <smy984@naver.com>
2022-08-08 23:24:59 +0900

    새

---
+++ selfstudy/src/baekjoon/Test15.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+public class Test15 {
+	public static void main(String[] args) {
+//		새싹을 출력하시오.
+
+//		        ,r'"7
+//		r`-_   ,'  ,/
+//		\. ". L_r'
+//		  `~\/
+//		     |
+//		     |
+		
+		System.out.println("\t,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println("\\. \". L_r\'");
+		System.out.println("  `~\\/");
+		System.out.println("     |");
+		System.out.println("     |");
+		
+	}
+}
